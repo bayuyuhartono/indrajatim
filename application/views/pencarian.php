4ef7c1047f35a936431e63e95210cd5e0c350200
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-09-01 11:04:43 +0700

    pencarian

--- application/views/pencarian.php
+++ application/views/pencarian.php
@@ -2,23 +2,23 @@
 <section id="page-content">    
 	<div class="container">        
 		<div class="page-title">            
-			<h2>Budaya</h2>        
+			<h2>Pencarian</h2>        
 		</div>        
 		<br>        
 		<div id="blog" class="grid-layout post-3-columns m-b-30" data-item="post-item">            
-			<?php foreach ($list_budaya as $data) { ?>
+			<?php foreach ($list_search as $data) { ?>
 				<div class="post-item border">                
 					<div class="post-item-wrap">                    
 						<div class="post-image">                        
-							<a href="<?= base_url('budaya/detail/'.$data['id_berita']);?>">                            
-								<img alt="img-budaya" src="<?= base_url('assets/admin/upload/berita/'.$data['gambar']);?>">
+							<a href="<?= base_url('berita/detail/'.$data['id_berita']);?>">                            
+								<img alt="img-berita" src="<?= base_url('assets/admin/upload/berita/'.$data['gambar']);?>">
 							</a>                    
 						</div>                    
 						<div class="post-item-description">                        
 							<span class="post-meta-date"><i class="fa fa-calendar"></i><?= date('d-m-Y', strtotime($data['tanggal']));?></span>                        
 							<span class="post-meta-comments"><i class="fa fa-user"></i>By Admin</span>                        
-							<h2 style="font-size:13px;"><a href="<?= base_url('budaya/detail/'.$data['id_berita']);?>"><?= $data['judul'];?></a></h2>                     
-							<a href="<?= base_url('budaya/detail/'.$data['id_berita']);?>" class="item-link">Read More <i class="icon-chevron-right"></i></a>              
+							<h2 style="font-size:13px;"><a href="<?= base_url('berita/detail/'.$data['id_berita']);?>"><?= $data['judul'];?></a></h2>                     
+							<a href="<?= base_url('berita/detail/'.$data['id_berita']);?>" class="item-link">Read More <i class="icon-chevron-right"></i></a>              
 						</div>                
 					</div>            
 				</div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -8,7 +8,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= $title;?></title>
     <link href="<?= base_url('assets/temp/css/plugins.css');?>" rel="stylesheet">
-    <link href="<?= base_url('assets/temp/css/style.css');?>" rel="stylesheet"> 
+    <link href="<?= base_url('assets/temp/css/style.css');?>" rel="stylesheet">
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 <style type="text/css">
     .struktur_new {
@@ -33,6 +36,92 @@
     .post-item-description h2 {
         height: 80px;
     }
+
+    /* searchbox */
+
+    input {
+        outline: none;
+    }
+    input[type=search] {
+        -webkit-appearance: textfield;
+        -webkit-box-sizing: content-box;
+        font-family: inherit;
+        font-size: 100%;
+    }
+    input::-webkit-search-decoration,
+    input::-webkit-search-cancel-button {
+        display: none; 
+    }
+
+
+    input[type=search] {
+        background: url(https://static.tumblr.com/ftv85bp/MIXmud4tx/search-icon.png) no-repeat 9px center;
+        border: none;
+        padding: 9px 10px 9px 32px;
+        width: 55px;
+        
+        -webkit-border-radius: 10em;
+        -moz-border-radius: 10em;
+        border-radius: 10em;
+        
+        -webkit-transition: all .5s;
+        -moz-transition: all .5s;
+        transition: all .5s;
+    }
+    input[type=search]:focus {
+        width: 130px;
+        background-color: #fff;
+        border-color: #66CC75;
+        
+        -webkit-box-shadow: 0 0 5px rgba(109,207,246,.5);
+        -moz-box-shadow: 0 0 5px rgba(109,207,246,.5);
+        box-shadow: 0 0 5px rgba(109,207,246,.5);
+    }
+
+
+    input:-moz-placeholder {
+        color: #999;
+    }
+    input::-webkit-input-placeholder {
+        color: #999;
+    }
+
+    /* Demo 2 */
+    #demo-2 input[type=search] {
+        width: 15px;
+        padding-left: 10px;
+        color: transparent;
+        cursor: pointer;
+    }
+    #demo-2 input[type=search]:hover {
+        background-color: #fff;
+    }
+    #demo-2 input[type=search]:focus {
+        width: 130px;
+        padding-left: 32px;
+        color: #000;
+        background-color: #fff;
+        cursor: auto;
+    }
+    #demo-2 input:-moz-placeholder {
+        color: transparent;
+    }
+    #demo-2 input::-webkit-input-placeholder {
+        color: transparent;
+    }
+
+    input:not([type=checkbox]):not([type=radio]),
+    select {
+        min-height: 15px !important;
+    }
+    
+    textarea:focus,
+    input[type="search"]:focus,
+    .uneditable-input:focus {   
+        border-color: none;
+        box-shadow: inset 0 1px 1px rgb(222, 222, 222), 0 0 8px rgb(222, 222, 222);
+        outline: 0 none;
+    }
 </style>
 <body>
     <div class="body-inner">
@@ -51,6 +140,11 @@
                         <div class="container">
                             <nav>
                                 <ul>
+                                    <li>
+                                        <form id="demo-2" method="post" action="<?= base_url('pencarian');?>">
+                                            <input type="search" class="search-form" name="searchvalue" autocomplete="off" placeholder="Search">
+                                        </form>
+                                    </li>
                                     <li><a href="<?= base_url('infoterkini');?>">Info Terkini</a></li>
                                     <li><a href="<?= base_url('kabarjatim');?>">Kabar Jatim</a></li>
                                     <li><a href="<?= base_url('politik');?>">Politik</a></li>
@@ -64,7 +158,7 @@
                                         <li><a href="<?= base_url('login/logout');?>">Logout</a></li>
                                     <?php }else{ ?>    
                                         <li><a class="btn btn-primary text-white rounded-pill" href="<?= base_url('login');?>">Login</a></li> 
-                                    <?php } ?>    
+                                    <?php } ?>   
                                 </ul>
                             </nav>
                         </div>
@@ -86,3 +180,53 @@
             <?php } ?>    
         </div>
     </div>   
+
+    <script>
+      $(document).ready(function () {
+          var submitIcon = $('.searchbox-icon');
+          var inputBox = $('.searchbox-input');
+          var searchBox = $('.searchbox');
+          var isOpen = false;
+          submitIcon.click(function () {
+              if (isOpen == false) {
+                  searchBox.addClass('searchbox-open');
+                  inputBox.focus();
+                  isOpen = true;
+              } else {
+                  searchBox.removeClass('searchbox-open');
+                  inputBox.focusout();
+                  isOpen = false;
+              }
+          });
+          submitIcon.mouseup(function () {
+              return false;
+          });
+          searchBox.mouseup(function () {
+              return false;
+          });
+          $(document).mouseup(function () {
+              if (isOpen == true) {
+                  $('.searchbox-icon').css('display', 'block');
+                  submitIcon.click();
+              }
+          });
+
+            // $('.search-form').keypress(function (e) {
+            //     if (e.which == 13) {
+            //         let searchvalue = e.target.value;
+            //         window.location.replace("<?= base_url('pencarian/berita');?>/" + searchvalue);
+            //     }
+            // });
+      });
+
+      function buttonUp() {
+          var inputVal = $('.searchbox-input').val();
+          inputVal = $.trim(inputVal).length;
+          if (inputVal !== 0) {
+              $('.searchbox-icon').css('display', 'none');
+          } else {
+              $('.searchbox-input').val('');
+              $('.searchbox-icon').css('display', 'block');
+          }
+      }
+    </script>
